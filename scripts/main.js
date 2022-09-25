78fce1fcc8410333a8e5a13d0f094275d823eb07
Tahiry Ratsimanohatra <kamaray.za@gmail.com>
2022-09-24 23:57:46 -0600

    add design principle page

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,23 @@
 let lastModified = document.lastModified;
 document.getElementById('timestamp').innerHTML = lastModified;
+
+let granimInstance = new Granim({
+  element: "#canvas-image-blending",
+  direction: "top-bottom",
+  isPausedWhenNotInView: true,
+  image: {
+    source: "../images/forest.jpeg",
+    blendingMode: "multiply",
+  },
+  states: {
+    "default-state": {
+      gradients: [
+        ["#29323c", "#485563"],
+        ["#FF6B6B", "#556270"],
+        ["#80d3fe", "#7ea0c4"],
+        ["#f0ab51", "#eceba3"],
+      ],
+      transitionSpeed: 6000,
+    },
+  },
+});
