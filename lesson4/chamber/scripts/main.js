56af0110254b2a46db6123e6e0f820e837e6d155
Tahiry Ratsimanohatra <kamaray.za@gmail.com>
2022-11-03 00:07:20 -0600

    lesson 7 discover

--- lesson4/chamber/scripts/main.js
+++ lesson4/chamber/scripts/main.js
@@ -8,3 +8,35 @@ x.onclick = toggleMenu;
 
 let date = new Date().toUTCString();
 document.getElementById("current").innerHTML = date;
+
+// Lazy loading
+const images = document.querySelectorAll("[src]");
+console.log(images);
+
+function preloadImage(img) {
+  const src = img.getAttribute("src");
+  if (!src) {
+    return;
+  }
+
+  img.src = src;
+}
+
+const imgOptions = {
+  threshold: 0,
+  rootMargin: "0px 0px 100px 0px",
+};
+const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      preloadImage(entry.target);
+      imgObserver.unobserve(entry.target);
+    }
+  });
+}, imgOptions);
+
+images.forEach((image) => {
+  imgObserver.observe(image);
+});
