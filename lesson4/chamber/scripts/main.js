c742264df3c770f09bddaffb80c63e4b5f197739
Tahiry Ratsimanohatra <93221051+Tahiry-MO@users.noreply.github.com>
2022-11-03 17:11:01 -0600

    visit count

--- lesson4/chamber/scripts/main.js
+++ lesson4/chamber/scripts/main.js
@@ -24,7 +24,7 @@ function preloadImage(img) {
 
 const imgOptions = {
   threshold: 1,
-  rootMargin: "0px 0px -500px 0px",
+  rootMargin: "0px 0px 100px 0px",
 };
 const imgObserver = new IntersectionObserver((entries, imgObserver) => {
   entries.forEach((entry) => {
@@ -40,3 +40,28 @@ const imgObserver = new IntersectionObserver((entries, imgObserver) => {
 images.forEach((image) => {
   imgObserver.observe(image);
 });
+
+// visit count
+const today = new Date();
+const userStat = {};
+userStat.currentDate = today;
+let visited = ""
+if (localStorage.getItem("visited")) {
+  	visited += localStorage.getItem("visited");
+} else {
+	localStorage.setItem("visited", today)
+	visited += localStorage.getItem("visited");
+}
+
+
+function dateDiffInDays(a, b) {
+  const _MS_PER_DAY = 1000 * 60 * 60 * 24;
+  // Discard the time and time-zone information.
+  const utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate());
+  const utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate());
+  return Math.floor((utc2 - utc1) / _MS_PER_DAY);
+}
+
+difference = dateDiffInDays(new Date(visited), today);
+
+document.getElementById("visit_count").textContent = difference;
