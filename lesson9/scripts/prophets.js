6731970ba23d1176c44123068eeac4107de7aaa6
Tahiry Ratsimanohatra <93221051+Tahiry-MO@users.noreply.github.com>
2022-11-10 11:54:53 -0700

    Update prophets.js

--- lesson9/scripts/prophets.js
+++ lesson9/scripts/prophets.js
@@ -9,31 +9,37 @@ fetch(requestURL)
   
 const prophets = JSON.parse(sessionStorage.prophets)['prophets']
 
-prophets.forEach(prophet => {
-  let node = document.createElement("div")
+function addProphets() {
+  prophets.forEach((prophet) => {
+    let node = document.createElement("div");
 
-  // name
-  let name = document.createElement("h2")
-  name.textContent = prophet['name'] + ' ' + prophet['lastname']
-  node.appendChild(name)
+    // name
+    let name = document.createElement("h2");
+    name.textContent = prophet["name"] + " " + prophet["lastname"];
+    node.appendChild(name);
 
-  // Date of Birth
-  let dob = document.createElement("p");
-  dob.textContent = "Date of Birth: " + prophet["birthdate"];
-  node.appendChild(dob);
+    // Date of Birth
+    let dob = document.createElement("p");
+    dob.textContent = "Date of Birth: " + prophet["birthdate"];
+    node.appendChild(dob);
 
-  // Place of Birth
-  let pob = document.createElement("p");
-  pob.textContent = "Place of Birth: " + prophet["birthplace"];
-  node.appendChild(pob);
+    // Place of Birth
+    let pob = document.createElement("p");
+    pob.textContent = "Place of Birth: " + prophet["birthplace"];
+    node.appendChild(pob);
 
-  // Profile image
-  let image = document.createElement('img')
-  image.setAttribute('src', prophet['imageurl'])
-  image.setAttribute('alt', `${prophet['name']} ${prophet['lastname']}'s picture`)
-  node.appendChild(image)
+    // Profile image
+    let image = document.createElement("img");
+    image.setAttribute("src", prophet["imageurl"]);
+    image.setAttribute(
+      "alt",
+      `${prophet["name"]} ${prophet["lastname"]}'s picture`
+    );
+    node.appendChild(image);
 
+    cards.appendChild(node);
+    console.log(prophet);
+  });
+}
 
-  cards.appendChild(node)
-  console.log(prophet)
-});
+addProphets();
