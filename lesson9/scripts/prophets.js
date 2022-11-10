94303807a7422eb4528ca5494c9cc4b6d84896a3
Tahiry Ratsimanohatra <93221051+Tahiry-MO@users.noreply.github.com>
2022-11-10 11:45:39 -0700

    lesson 9

--- lesson9/scripts/prophets.js
+++ lesson9/scripts/prophets.js
@@ -7,5 +7,33 @@ fetch(requestURL)
   .then((res) => res.json())
   .then((data) => { sessionStorage.setItem('prophets', JSON.stringify(data)) })
   
-const prophets = JSON.parse(sessionStorage.prophets)
-console.log(prophets['prophets'])
+const prophets = JSON.parse(sessionStorage.prophets)['prophets']
+
+prophets.forEach(prophet => {
+  let node = document.createElement("div")
+
+  // name
+  let name = document.createElement("h2")
+  name.textContent = prophet['name'] + ' ' + prophet['lastname']
+  node.appendChild(name)
+
+  // Date of Birth
+  let dob = document.createElement("p");
+  dob.textContent = "Date of Birth: " + prophet["birthdate"];
+  node.appendChild(dob);
+
+  // Place of Birth
+  let pob = document.createElement("p");
+  pob.textContent = "Place of Birth: " + prophet["birthplace"];
+  node.appendChild(pob);
+
+  // Profile image
+  let image = document.createElement('img')
+  image.setAttribute('src', prophet['imageurl'])
+  image.setAttribute('alt', `${prophet['name']} ${prophet['lastname']}'s picture`)
+  node.appendChild(image)
+
+
+  cards.appendChild(node)
+  console.log(prophet)
+});
