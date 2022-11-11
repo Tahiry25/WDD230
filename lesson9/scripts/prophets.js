dd03f7f4a608e3fdf8c6c77838819374e77d5c86
Tahiry Ratsimanohatra <kamaray.za@gmail.com>
2022-11-10 22:10:24 -0700

    Update prophets.js

--- lesson9/scripts/prophets.js
+++ lesson9/scripts/prophets.js
@@ -1,47 +1,41 @@
 const requestURL =
   "https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json";
-const cards = document.querySelector(".cards");
 
+const cards = document.querySelector(".cards");
 
 fetch(requestURL)
   .then((res) => res.json())
   .then((data) => {
-    localStorage.setItem('prophets', JSON.stringify(data))
+    const prophets = data['prophets']
+    return prophets
+  }).then((prophets) => {
+    prophets.forEach((prophet) => {
+      let node = document.createElement("div");
+
+      // name
+      let name = document.createElement("h2");
+      name.textContent = prophet["name"] + " " + prophet["lastname"];
+      node.appendChild(name);
+
+      // Date of Birth
+      let dob = document.createElement("p");
+      dob.textContent = "Date of Birth: " + prophet["birthdate"];
+      node.appendChild(dob);
+
+      // Place of Birth
+      let pob = document.createElement("p");
+      pob.textContent = "Place of Birth: " + prophet["birthplace"];
+      node.appendChild(pob);
+
+      // Profile image
+      let image = document.createElement("img");
+      image.setAttribute("src", prophet["imageurl"]);
+      image.setAttribute(
+        "alt",
+        `${prophet["name"]} ${prophet["lastname"]}'s picture`
+      );
+      node.appendChild(image);
+
+      cards.appendChild(node);
+    });
   })
-
-
-function addProphets() {
-  const prophets = JSON.parse(localStorage.prophets)["prophets"];
-  prophets.forEach((prophet) => {
-    let node = document.createElement("div");
-
-    // name
-    let name = document.createElement("h2");
-    name.textContent = prophet["name"] + " " + prophet["lastname"];
-    node.appendChild(name);
-
-    // Date of Birth
-    let dob = document.createElement("p");
-    dob.textContent = "Date of Birth: " + prophet["birthdate"];
-    node.appendChild(dob);
-
-    // Place of Birth
-    let pob = document.createElement("p");
-    pob.textContent = "Place of Birth: " + prophet["birthplace"];
-    node.appendChild(pob);
-
-    // Profile image
-    let image = document.createElement("img");
-    image.setAttribute("src", prophet["imageurl"]);
-    image.setAttribute(
-      "alt",
-      `${prophet["name"]} ${prophet["lastname"]}'s picture`
-    );
-    node.appendChild(image);
-
-    cards.appendChild(node);
-    console.log(prophet);
-  });
-}
-
-addProphets();
